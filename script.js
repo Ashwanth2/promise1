09f03ba3e1c7dab19c8bc6cdd1aa0b12ed643d80
Ashwanth2 <121960023+Ashwanth2@users.noreply.github.com>
2023-04-21 16:54:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 
-
-var req = fetch('https://api-thirukkural.vercel.app/api?num=1')
-.then((res)=>res.json())
-.then((data)=> data.line1,data.line2)
-console.log(data.line1,data.line2)
-
-function printMessage(a,b) {
-   document.getElementById('output').innerHTML= a,b
-}
-
 function printMessage(){        
     var req = fetch("https://api-thirukkural.vercel.app/api?num=1")
         .then((res) => res.json())
